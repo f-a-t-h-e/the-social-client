86447715c21ce33181399f84a16d1b4145c5fa35
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-02-12 06:23:04 +0200

    Create LoginPage starter.

--- src/scenes/loginPage/index.tsx
+++ src/scenes/loginPage/index.tsx
@@ -1,9 +1,30 @@
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
 import React from "react";
 
 type Props = {};
 
 const LoginPage = (props: Props) => {
-  return <div>LoginPage</div>;
+  const theme = useTheme();
+  const isNonMobileScreen = useMediaQuery("(min-width: 1000px)");
+
+  // @ts-ignore
+  const neutralLight = theme.palette.neutral.light;
+  // @ts-ignore
+  const dark = theme.palette.neutral.dark;
+  const background = theme.palette.background.default;
+  const primaryLight = theme.palette.primary.light;
+  // @ts-ignore
+  const alt = theme.palette.background.alt;
+
+  return (
+    <Box>
+      <Box width="100%" bgcolor={alt} p="1rem 6%" textAlign="center">
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          The-Social
+        </Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default LoginPage;
